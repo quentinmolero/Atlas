601809847aebdcdffb967c1f077cb621a3fd5565
quentinmolero <quentin.molero@outlook.com>
2024-09-09 21:04:21 +0200

    First Implementation of login button and onClick event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MapComponent} from "./features/map/map/map.component";
+import {MapComponent} from "./features/map/map.component";
+import {LoginComponent} from "./features/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MapComponent],
+  imports: [RouterOutlet, MapComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
