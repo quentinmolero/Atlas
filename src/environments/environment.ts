f62f84c9d403458fd77cbfdb1c0528cae1bebc6d
quentinmolero <quentin.molero@outlook.com>
2024-12-16 21:38:56 +0100

    Minor CSS fix

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080',
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiZnJhbnNrbWFuIiwiYSI6ImNqdWZlNno1dzBjOHE0NHBxbWZyaWp5dGMifQ.YfIJ2rV_oVaPe9j_b11e1A'
+  }
+};
