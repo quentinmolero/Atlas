e231040ca98915c8f6bca399563d9fb7f49307ff
quentinmolero <quentin.molero@outlook.com>
2025-01-13 09:22:00 +0100

    WIP implementing geocoding

--- src/app/features/location-input/location-input.component.ts
+++ src/app/features/location-input/location-input.component.ts
@@ -1,6 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AddLocationPopupServiceService} from "../../services/display/add-location-popup-service.service";
 import {FormsModule} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {debounceTime, distinctUntilChanged, Subject, switchMap} from "rxjs";
 
 @Component({
   selector: 'app-location-input',
@@ -11,15 +14,37 @@ import {FormsModule} from "@angular/forms";
   templateUrl: './location-input.component.html',
   styleUrl: './location-input.component.css'
 })
-export class LocationInputComponent {
+export class LocationInputComponent implements OnInit{
   public searchText: string = "";
+  private searchTerms = new Subject<string>();
+  searchResults: any[] = [];
   constructor(
     private _addLocationPopupService: AddLocationPopupServiceService,
+    private http: HttpClient,
   ) {
   }
 
   public onInputChange(text: string): void {
     this.searchText = text;
-    console.log(this.searchText);
+    this.search(this.searchText);
+  }
+
+  public searchLocation(location: string) {
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${environment.mapbox.accessToken}`;
+    return this.http.get(url);
+  }
+
+  ngOnInit() {
+    this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.searchLocation(term))
+    ).subscribe(results => {
+      console.log(results);
+    });
+  }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 }
