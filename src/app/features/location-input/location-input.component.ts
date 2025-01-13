77b7b094db095acc94731a7c713ee7921f39393f
quentinmolero <quentin.molero@outlook.com>
2025-01-13 08:35:21 +0100

    Live detection of input text in location-input component

--- src/app/features/location-input/location-input.component.ts
+++ src/app/features/location-input/location-input.component.ts
@@ -1,16 +1,25 @@
 import {Component} from '@angular/core';
 import {AddLocationPopupServiceService} from "../../services/display/add-location-popup-service.service";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-location-input',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './location-input.component.html',
   styleUrl: './location-input.component.css'
 })
 export class LocationInputComponent {
+  public searchText: string = "";
   constructor(
     private _addLocationPopupService: AddLocationPopupServiceService,
   ) {
+  }
+
+  public onInputChange(text: string): void {
+    this.searchText = text;
+    console.log(this.searchText);
   }
 }
